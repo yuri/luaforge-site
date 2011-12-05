207aa239bb5bc7b0b722d0accde852f47114a3e6
Yuri Takhteyev <yuri@freewisdom.org>
2011-12-05 02:07:44 -0500

    Updating LuaCom record per David Manura's request.

--- projects/luacom.lua
+++ projects/luacom.lua
@@ -14,9 +14,9 @@ abstract = [[
 LuaCOM is an add-on library to the Lua language that allows Lua programs to use and implement objects that follow Microsoft's Component Object Model (COM) specification and use the ActiveX technology for property access and method calls.
 ]]
 license = "Other/Proprietary License"
-language = "c++, lua 4, lua 5"
-tags = "software development"
+language = "c++, lua 5.1, lua 5.2"
+tags = "binding"
 activity = "70.19%"
 registered = "2005-01-10 16:12"
 os = "windows"
-website = "http://www.tecgraf.puc-rio.br/~rcerq/luacom"
+website = "http://lua-users.org/wiki/LuaCom"
