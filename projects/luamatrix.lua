0cbb18a350c375c9dc275b864bfe2e3be73ccb8d
Yuri Takhteyev <yuri@freewisdom.org>
2011-12-05 02:05:12 -0500

    Changing language for LuaMatrix and adding a source code repository.

--- projects/luamatrix.lua
+++ projects/luamatrix.lua
@@ -11,9 +11,10 @@ abstract = [[
 Source code at http://github.com/davidm/lua-matrix.
 ]]
 license = "MIT/X"
-language = "lua 5"
+language = "lua 5.1, lua 5.2"
 tags = "mathematics"
 activity = "0%"
 registered = "2007-08-15 17:04"
 os = "os independent"
 website = "http://lua-users.org/wiki/LuaMatrix"
+source=[[https://github.com/davidm/lua-matrix]]
