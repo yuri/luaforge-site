f65136914c5c2dd406f6f929dcc9aaad3a2e7581
Yuri Takhteyev <yuri@freewisdom.org>
2013-03-13 16:32:21 -0400

    Adding a new abstract to BTD, submitted by the maintainer.

--- projects/btd.lua
+++ projects/btd.lua
@@ -6,12 +6,15 @@ releases = [[
 owners = "wimlangers"
 creator = "wimlangers"
 abstract = [[
-LuaUnit seems to be gone so enter the 'build, test, deploy development environment suite' : LuaUnit compatible, IUP based GUI testing, sandbox development environment, code checks, compile, ...
+Great plans... Little time... and actually what is there to Build or Deploy in a dynamic language?
+Should have read "Java - Build, Test, Deploy" but Lua lured me into dropping Java (which was outgrowing??? me).
+jUnit inspired unit testing (originally LuaUnit based) is the only production quality component.
+There is also a (windows only) IUP robot.
 ]]
 license = "MIT/X"
 language = "c, lua 5"
-tags = "build tools, testing tools"
+tags = "testing tools"
 activity = "0%"
 registered = "2009-02-16 22:47"
-os = "macos, windows nt/2000, linux"
+os = "cross-platform"
 website = " http://users.skynet.be/adrias/Lua/index.html"
