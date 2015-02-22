bc9b616bede28b98cfb9c1d906d8ee99c05a6038
Yuri Takhteyev <yuri@yto.io>
2015-02-22 01:33:26 -0500

    Updating grunt (now Winsh) with the new title.

--- projects/grunt.lua
+++ projects/grunt.lua
@@ -9,7 +9,10 @@ releases = [[
 owners = "fsiamp,johnhind"
 creator = "fsiamp,johnhind"
 abstract = [[
-Grunt is an implementation of Lua with extensions for Windows Shell Scripting. It generates a system tray icon and context menu defined in Lua. The menu items invoke Lua functions which can perform most of the commands of Windows Explorer and more.
+Grunt has been renamed "Winsh.lua" and is maintained at the GitHub repository referenced below.
+Winsh is an implementation of Lua with extensions for Windows Shell Scripting.
+It generates a system tray icon and context menu defined in Lua.
+The menu items invoke Lua functions which can perform most of the commands of Windows Explorer and more.
 ]]
 license = "GPL"
 language = "c++, lua 5"
@@ -17,4 +20,4 @@ tags = "window managers, system shells"
 activity = "0%"
 registered = "2009-02-13 12:17"
 os = "windows"
-website = "http://grunt.luaforge.net"
+website = "https://github.com/JohnHind/Winsh.lua"
